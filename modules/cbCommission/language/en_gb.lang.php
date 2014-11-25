8ce68ef9c322903a912730eb76979d6af3e1ef17
Joe Bordes <joe@tsolucio.com>
2014-11-25 20:27:31 +0100

    cbCommission module

---
+++ modules/cbCommission/language/en_gb.lang.php
@@ -0,0 +1,30 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'cbCommission' => 'Commission',
+	'SINGLE_cbCommission' => 'Commission',
+	'cbCommission ID' => 'Commission ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_CBCOMMISSION_INFORMATION' => 'Commission Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+
+	'cbcommissionno' => 'Commission No',
+	'percentage' => 'Percentage',
+	'amount' => 'Amount',
+	'soinvtotal' => 'Document Total',
+	'commission' => 'Commission',
+	'employeeid' => 'Employee',
+	'soinvid' => 'Document',
+
+);
+
+?>
---
+++ modules/cbCommission/language/es_mx.lang.php
@@ -0,0 +1,30 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'cbCommission' => 'Comisión',
+	'SINGLE_cbCommission' => 'Comisión',
+	'cbCommission ID' => 'ID Comisión',
+
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_CBCOMMISSION_INFORMATION' => 'Información Comisión',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
+
+	'cbcommissionno' => 'Nº Comisión',
+	'percentage' => 'Porcentaje',
+	'amount' => 'Cantidad',
+	'soinvtotal' => 'Total Documento',
+	'commission' => 'Comisión',
+	'employeeid' => 'Empleado',
+	'soinvid' => 'Documento',
+
+);
+
+?>
