50bcc0f68a0e671efc99adf90c34ccf4c3567974
Kevin Duqi <k.duqi@studiosynthesis.biz>
2015-10-09 17:18:46 +0200

    fix() sync module code with vtlib base module code

--- modules/cbCommission/cbCommission.php
+++ modules/cbCommission/cbCommission.php
@@ -24,6 +24,8 @@ class cbCommission extends CRMEntity {
 	 * Mandatory table for supporting custom fields.
 	 */
 	var $customFieldTable = Array('vtiger_cbcommissioncf', 'cbcommissionid');
+	// Uncomment the line below to support custom field columns on related lists
+	// var $related_tables = Array('vtiger_cbcommissioncf'=>array('cbcommissionid','vtiger_cbcommission', 'cbcommissionid'));
 
 	/**
 	 * Mandatory for Saving, Include tables related to this module.
@@ -36,33 +38,33 @@ class cbCommission extends CRMEntity {
 	var $tab_name_index = Array(
 		'vtiger_crmentity' => 'crmid',
 		'vtiger_cbcommission'   => 'cbcommissionid',
-	    'vtiger_cbcommissioncf' => 'cbcommissionid');
+		'vtiger_cbcommissioncf' => 'cbcommissionid');
 
 	/**
 	 * Mandatory for Listing (Related listview)
 	 */
 	var $list_fields = Array (
-		/* Format: Field Label => Array(tablename, columnname) */
+		/* Format: Field Label => Array(tablename => columnname) */
 		// tablename should not have prefix 'vtiger_'
-		'cbcommissionno'=> Array('cbcommission', 'cbcommissionno'),
-		'employeeid'=> Array('cbcommission', 'employeeid'),
-		'soinvid'=> Array('cbcommission', 'soinvid'),
-		'percentage'=> Array('cbcommission', 'percentage'),
-		'amount'=> Array('cbcommission', 'amount'),
-		'soinvtotal'=> Array('cbcommission', 'soinvtotal'),
-		'commission'=> Array('cbcommission', 'commission'),
-		'Assigned To' => Array('crmentity','smownerid')
+		'cbcommissionno'=> Array('cbcommission' => 'cbcommissionno'),
+		'employeeid'    => Array('cbcommission' => 'employeeid'),
+		'soinvid'       => Array('cbcommission' => 'soinvid'),
+		'percentage'    => Array('cbcommission' => 'percentage'),
+		'amount'        => Array('cbcommission' => 'amount'),
+		'soinvtotal'    => Array('cbcommission' => 'soinvtotal'),
+		'commission'    => Array('cbcommission' => 'commission'),
+		'Assigned To'   => Array('crmentity' => 'smownerid')
 	);
 	var $list_fields_name = Array(
 		/* Format: Field Label => fieldname */
 		'cbcommissionno'=> 'cbcommissionno',
-		'employeeid'=> 'employeeid',
-		'soinvid'=> 'soinvid',
-		'percentage'=> 'percentage',
-		'amount'=> 'amount',
-		'soinvtotal'=> 'soinvtotal',
-		'commission'=> 'commission',
-		'Assigned To' => 'assigned_user_id'
+		'employeeid'    => 'employeeid',
+		'soinvid'       => 'soinvid',
+		'percentage'    => 'percentage',
+		'amount'        => 'amount',
+		'soinvtotal'    => 'soinvtotal',
+		'commission'    => 'commission',
+		'Assigned To'   => 'assigned_user_id'
 	);
 
 	// Make the field link to detail view from list view (Fieldname)
@@ -70,25 +72,25 @@ class cbCommission extends CRMEntity {
 
 	// For Popup listview and UI type support
 	var $search_fields = Array(
-		/* Format: Field Label => Array(tablename, columnname) */
+		/* Format: Field Label => Array(tablename => columnname) */
 		// tablename should not have prefix 'vtiger_'
-		'cbcommissionno'=> Array('cbcommission', 'cbcommissionno'),
-		'employeeid'=> Array('cbcommission', 'employeeid'),
-		'soinvid'=> Array('cbcommission', 'soinvid'),
-		'percentage'=> Array('cbcommission', 'percentage'),
-		'amount'=> Array('cbcommission', 'amount'),
-		'soinvtotal'=> Array('cbcommission', 'soinvtotal'),
-		'commission'=> Array('cbcommission', 'commission'),
+		'cbcommissionno'=> Array('cbcommission' => 'cbcommissionno'),
+		'employeeid'    => Array('cbcommission' => 'employeeid'),
+		'soinvid'       => Array('cbcommission' => 'soinvid'),
+		'percentage'    => Array('cbcommission' => 'percentage'),
+		'amount'        => Array('cbcommission' => 'amount'),
+		'soinvtotal'    => Array('cbcommission' => 'soinvtotal'),
+		'commission'    => Array('cbcommission' => 'commission')
 	);
 	var $search_fields_name = Array(
 		/* Format: Field Label => fieldname */
 		'cbcommissionno'=> 'cbcommissionno',
-		'employeeid'=> 'employeeid',
-		'soinvid'=> 'soinvid',
-		'percentage'=> 'percentage',
-		'amount'=> 'amount',
-		'soinvtotal'=> 'soinvtotal',
-		'commission'=> 'commission',
+		'employeeid'    => 'employeeid',
+		'soinvid'       => 'soinvid',
+		'percentage'    => 'percentage',
+		'amount'        => 'amount',
+		'soinvtotal'    => 'soinvtotal',
+		'commission'    => 'commission'
 	);
 
 	// For Popup window record selection
@@ -114,38 +116,18 @@ class cbCommission extends CRMEntity {
 	// Used when enabling/disabling the mandatory fields for the module.
 	// Refers to vtiger_field.fieldname values.
 	var $mandatory_fields = Array('createdtime', 'modifiedtime');
-	
+
 	function __construct() {
 		global $log, $currentModule;
 		$this->column_fields = getColumnFields($currentModule);
 		$this->db = PearDatabase::getInstance();
 		$this->log = $log;
-	}
-
-	function getSortOrder() {
-		global $currentModule;
-
-		$sortorder = $this->default_sort_order;
-		if($_REQUEST['sorder']) $sortorder = $this->db->sql_escape_string($_REQUEST['sorder']);
-		else if($_SESSION[$currentModule.'_Sort_Order']) 
-			$sortorder = $_SESSION[$currentModule.'_Sort_Order'];
-
-		return $sortorder;
-	}
-
-	function getOrderBy() {
-		global $currentModule;
-		
-		$use_default_order_by = '';		
-		if(PerformancePrefs::getBoolean('LISTVIEW_DEFAULT_SORTING', true)) {
-			$use_default_order_by = $this->default_order_by;
+		$sql = 'SELECT 1 FROM vtiger_field WHERE uitype=69 and tabid = ?';
+		$tabid = getTabid($currentModule);
+		$result = $this->db->pquery($sql, array($tabid));
+		if ($result and $this->db->num_rows($result)==1) {
+			$this->HasDirectImageField = true;
 		}
-		
-		$orderby = $use_default_order_by;
-		if($_REQUEST['order_by']) $orderby = $this->db->sql_escape_string($_REQUEST['order_by']);
-		else if($_SESSION[$currentModule.'_Order_By'])
-			$orderby = $_SESSION[$currentModule.'_Order_By'];
-		return $orderby;
 	}
 
 	function save_module($module) {
@@ -182,7 +164,7 @@ class cbCommission extends CRMEntity {
 	 */
 	function getListQuery($module, $usewhere='') {
 		$query = "SELECT vtiger_crmentity.*, $this->table_name.*";
-		
+
 		// Keep track of tables joined to avoid duplicates
 		$joinedTables = array();
 
@@ -195,32 +177,32 @@ class cbCommission extends CRMEntity {
 
 		$joinedTables[] = $this->table_name;
 		$joinedTables[] = 'vtiger_crmentity';
-		
+
 		// Consider custom table join as well.
 		if(!empty($this->customFieldTable)) {
 			$query .= " INNER JOIN ".$this->customFieldTable[0]." ON ".$this->customFieldTable[0].'.'.$this->customFieldTable[1] .
-				      " = $this->table_name.$this->table_index";
-			$joinedTables[] = $this->customFieldTable[0]; 
+				" = $this->table_name.$this->table_index";
+			$joinedTables[] = $this->customFieldTable[0];
 		}
 		$query .= " LEFT JOIN vtiger_users ON vtiger_users.id = vtiger_crmentity.smownerid";
 		$query .= " LEFT JOIN vtiger_groups ON vtiger_groups.groupid = vtiger_crmentity.smownerid";
 
 		$joinedTables[] = 'vtiger_users';
 		$joinedTables[] = 'vtiger_groups';
-		
+
 		$linkedModulesQuery = $this->db->pquery("SELECT distinct fieldname, columnname, relmodule FROM vtiger_field" .
 				" INNER JOIN vtiger_fieldmodulerel ON vtiger_fieldmodulerel.fieldid = vtiger_field.fieldid" .
 				" WHERE uitype='10' AND vtiger_fieldmodulerel.module=?", array($module));
 		$linkedFieldsCount = $this->db->num_rows($linkedModulesQuery);
-		
+
 		for($i=0; $i<$linkedFieldsCount; $i++) {
 			$related_module = $this->db->query_result($linkedModulesQuery, $i, 'relmodule');
 			$fieldname = $this->db->query_result($linkedModulesQuery, $i, 'fieldname');
 			$columnname = $this->db->query_result($linkedModulesQuery, $i, 'columnname');
-			
-			$other =  CRMEntity::getInstance($related_module);
+
+			$other = CRMEntity::getInstance($related_module);
 			vtlib_setup_modulevars($related_module, $other);
-			
+
 			if(!in_array($other->table_name, $joinedTables)) {
 				$query .= " LEFT JOIN $other->table_name ON $other->table_name.$other->table_index = $this->table_name.$columnname";
 				$joinedTables[] = $other->table_name;
@@ -244,7 +226,7 @@ class cbCommission extends CRMEntity {
 		$sec_query = '';
 		$tabid = getTabid($module);
 
-		if($is_admin==false && $profileGlobalPermission[1] == 1 && $profileGlobalPermission[2] == 1 
+		if($is_admin==false && $profileGlobalPermission[1] == 1 && $profileGlobalPermission[2] == 1
 			&& $defaultOrgSharingPermission[$tabid] == 3) {
 
 				$sec_query .= " AND (vtiger_crmentity.smownerid in($current_user->id) OR vtiger_crmentity.smownerid IN 
@@ -259,9 +241,8 @@ class cbCommission extends CRMEntity {
 						SELECT shareduserid FROM vtiger_tmp_read_user_sharing_per 
 						WHERE userid=".$current_user->id." AND tabid=".$tabid."
 					) 
-					OR 
-						(";
-		
+					OR (";
+
 					// Build the query based on the group association of current user.
 					if(sizeof($current_user_groups) > 0) {
 						$sec_query .= " vtiger_groups.groupid IN (". implode(",", $current_user_groups) .") OR ";
@@ -285,25 +266,25 @@ class cbCommission extends CRMEntity {
 	{
 		global $current_user;
 		$thismodule = $_REQUEST['module'];
-		
+
 		include("include/utils/ExportUtils.php");
 
 		//To get the Permitted fields query and the permitted fields list
 		$sql = getPermittedFieldsQuery($thismodule, "detail_view");
-		
+
 		$fields_list = getFieldsListFromQuery($sql);
 
 		$query = "SELECT $fields_list, vtiger_users.user_name AS user_name 
-					FROM vtiger_crmentity INNER JOIN $this->table_name ON vtiger_crmentity.crmid=$this->table_name.$this->table_index";
+				FROM vtiger_crmentity INNER JOIN $this->table_name ON vtiger_crmentity.crmid=$this->table_name.$this->table_index";
 
 		if(!empty($this->customFieldTable)) {
 			$query .= " INNER JOIN ".$this->customFieldTable[0]." ON ".$this->customFieldTable[0].'.'.$this->customFieldTable[1] .
-				      " = $this->table_name.$this->table_index"; 
+				" = $this->table_name.$this->table_index";
 		}
 
 		$query .= " LEFT JOIN vtiger_groups ON vtiger_groups.groupid = vtiger_crmentity.smownerid";
 		$query .= " LEFT JOIN vtiger_users ON vtiger_crmentity.smownerid = vtiger_users.id and vtiger_users.status='Active'";
-		
+
 		$linkedModulesQuery = $this->db->pquery("SELECT distinct fieldname, columnname, relmodule FROM vtiger_field" .
 				" INNER JOIN vtiger_fieldmodulerel ON vtiger_fieldmodulerel.fieldid = vtiger_field.fieldid" .
 				" WHERE uitype='10' AND vtiger_fieldmodulerel.module=?", array($thismodule));
@@ -314,10 +295,10 @@ class cbCommission extends CRMEntity {
 			$related_module = $this->db->query_result($linkedModulesQuery, $i, 'relmodule');
 			$fieldname = $this->db->query_result($linkedModulesQuery, $i, 'fieldname');
 			$columnname = $this->db->query_result($linkedModulesQuery, $i, 'columnname');
-			
+
 			$other = CRMEntity::getInstance($related_module);
 			vtlib_setup_modulevars($related_module, $other);
-			
+
 			if($rel_mods[$other->table_name]) {
 				$rel_mods[$other->table_name] = $rel_mods[$other->table_name] + 1;
 				$alias = $other->table_name.$rel_mods[$other->table_name];
@@ -325,9 +306,9 @@ class cbCommission extends CRMEntity {
 			} else {
 				$alias = $other->table_name;
 				$query_append = '';
-				$rel_mods[$other->table_name] = 1;	
+				$rel_mods[$other->table_name] = 1;
 			}
-			
+
 			$query .= " LEFT JOIN $other->table_name $query_append ON $alias.$other->table_index = $this->table_name.$columnname";
 		}
 
@@ -372,16 +353,16 @@ class cbCommission extends CRMEntity {
 		global $adb;
 		$count = 0;
 		$query1 = "select bean_id from vtiger_users_last_import where assigned_user_id=? AND bean_type='$module' AND deleted=0";
-		$result1 = $adb->pquery($query1, array($user_id)) or die("Error getting last import for undo: ".mysql_error()); 
+		$result1 = $adb->pquery($query1, array($user_id)) or die("Error getting last import for undo: ".mysql_error());
 		while ( $row1 = $adb->fetchByAssoc($result1))
 		{
 			$query2 = "update vtiger_crmentity set deleted=1 where crmid=?";
-			$result2 = $adb->pquery($query2, array($row1['bean_id'])) or die("Error undoing last import: ".mysql_error()); 
-			$count++;			
+			$result2 = $adb->pquery($query2, array($row1['bean_id'])) or die("Error undoing last import: ".mysql_error());
+			$count++;
 		}
 		return $count;
 	}
-	
+
 	/**
 	 * Transform the value while exporting
 	 */
@@ -396,12 +377,12 @@ class cbCommission extends CRMEntity {
 	{
 		global $current_user, $adb;
 		$record_user = $this->column_fields["assigned_user_id"];
-		
+
 		if($record_user != $current_user->id){
 			$sqlresult = $adb->pquery("select id from vtiger_users where id = ? union select groupid as id from vtiger_groups where groupid = ?", array($record_user, $record_user));
 			if($this->db->num_rows($sqlresult)!= 1) {
 				$this->column_fields["assigned_user_id"] = $current_user->id;
-			} else {			
+			} else {
 				$row = $adb->fetchByAssoc($sqlresult, -1, false);
 				if (isset($row['id']) && $row['id'] != -1) {
 					$this->column_fields["assigned_user_id"] = $row['id'];
@@ -411,8 +392,8 @@ class cbCommission extends CRMEntity {
 			}
 		}
 	}
-	
-	/** 
+
+	/**
 	 * Function which will give the basic query to find duplicates
 	 */
 	function getDuplicatesQuery($module,$table_cols,$field_values,$ui_type_arr,$select_cols='') {
@@ -423,39 +404,38 @@ class cbCommission extends CRMEntity {
 
 		$from_clause = " FROM $this->table_name";
 
-		$from_clause .= "	INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = $this->table_name.$this->table_index";
+		$from_clause .= " INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = $this->table_name.$this->table_index";
 
 		// Consider custom table join as well.
 		if(isset($this->customFieldTable)) {
 			$from_clause .= " INNER JOIN ".$this->customFieldTable[0]." ON ".$this->customFieldTable[0].'.'.$this->customFieldTable[1] .
-				      " = $this->table_name.$this->table_index"; 
+				" = $this->table_name.$this->table_index";
 		}
 		$from_clause .= " LEFT JOIN vtiger_users ON vtiger_users.id = vtiger_crmentity.smownerid
 						LEFT JOIN vtiger_groups ON vtiger_groups.groupid = vtiger_crmentity.smownerid";
-		
-		$where_clause = "	WHERE vtiger_crmentity.deleted = 0";
+
+		$where_clause = " WHERE vtiger_crmentity.deleted = 0";
 		$where_clause .= $this->getListViewSecurityParameter($module);
-					
+
 		if (isset($select_cols) && trim($select_cols) != '') {
-			$sub_query = "SELECT $select_cols FROM  $this->table_name AS t " .
+			$sub_query = "SELECT $select_cols FROM $this->table_name AS t " .
 				" INNER JOIN vtiger_crmentity AS crm ON crm.crmid = t.".$this->table_index;
 			// Consider custom table join as well.
 			if(isset($this->customFieldTable)) {
 				$sub_query .= " LEFT JOIN ".$this->customFieldTable[0]." tcf ON tcf.".$this->customFieldTable[1]." = t.$this->table_index";
 			}
-			$sub_query .= " WHERE crm.deleted=0 GROUP BY $select_cols HAVING COUNT(*)>1";	
+			$sub_query .= " WHERE crm.deleted=0 GROUP BY $select_cols HAVING COUNT(*)>1";
 		} else {
 			$sub_query = "SELECT $table_cols $from_clause $where_clause GROUP BY $table_cols HAVING COUNT(*)>1";
-		}	
-		
-		
+		}
+
 		$query = $select_clause . $from_clause .
 					" LEFT JOIN vtiger_users_last_import ON vtiger_users_last_import.bean_id=" . $this->table_name .".".$this->table_index .
 					" INNER JOIN (" . $sub_query . ") AS temp ON ".get_on_clause($field_values,$ui_type_arr,$module) .
 					$where_clause .
 					" ORDER BY $table_cols,". $this->table_name .".".$this->table_index ." ASC";
-					
-		return $query;		
+
+		return $query;
 	}
 
 	/**
@@ -487,13 +467,13 @@ class cbCommission extends CRMEntity {
 		}
 	}
 
-	/** 
+	/**
 	 * Handle saving related module information.
 	 * NOTE: This function has been added to CRMEntity (base class).
 	 * You can override the behavior by re-defining it here.
 	 */
 	// function save_related_module($module, $crmid, $with_module, $with_crmid) { }
-	
+
 	/**
 	 * Handle deleting related module information.
 	 * NOTE: This function has been added to CRMEntity (base class).
